332305c0c1b59bf924650949e9e6fa670a14f82e
RussellWaring <russell.waring@dcmail.ca>
2021-11-05 21:29:39 -0400

    Added the Person Class

---
+++ src/ca/durhamcollege/Person.java
@@ -0,0 +1,82 @@
+package ca.durhamcollege;
+
+public class Person
+{
+    // Private Instance Variables
+
+    private String m_name;
+    private int m_age;
+
+    // Public Properties
+
+    public String getName()
+    {
+        return m_name;
+    }
+
+    public void setName(String name)
+    {
+        this.m_name = name;
+    }
+
+    public int getAge()
+    {
+        return m_age;
+    }
+
+    public void setAge(int age)
+    {
+        this.m_age = age;
+    }
+
+    // Constructors
+
+    public Person()
+    {
+        this.m_name = "unknown";
+        this.m_age = 0;
+    }
+
+    public Person(String name, int age)
+    {
+        this.m_name = name;
+        this.m_age = age;
+    }
+
+    public Person(String name)
+    {
+        this.m_name = name;
+        this.m_age = 0;
+    }
+
+    public Person(int age)
+    {
+        this.m_name = "unknown";
+        this.m_age = age;
+    }
+
+    // Methods
+
+    /**
+     * This method tests the Person class and returns a simple message to the console
+     * @return {Void}
+     */
+    public void saysHello()
+    {
+        System.out.println(this.m_name + " says Hello!");
+    }
+
+    /** (A Java Doc)
+     * This method overrides the built-in toString method and returns the Person properties
+     * @return {String}
+     */
+    @Override
+    public String toString()
+    {
+        String outputString = "";
+        outputString += "Name: " + this.m_name + " ";
+        outputString += "Age : " + this.m_age;
+
+        return outputString;
+    }
+}
