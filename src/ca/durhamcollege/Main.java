7d4100b436a9d22cc8cebd4cee75476001f47633
RussellWaring <russell.waring@dcmail.ca>
2021-11-05 21:40:14 -0400

    Updated comment headers

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,12 +1,16 @@
+/**
+ * @author  Russell Waring, StudentID: 100528426
+ * @date    November 5th, 2021
+ * @version 0.0.1
+ *          Following along to the Fall 2020 video, for in-class exercise seven.
+ */
 package ca.durhamcollege;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
         Person person = new Person("Russell", 26);
         person.saysHello();
-
     }
 }
--- src/ca/durhamcollege/Person.java
+++ src/ca/durhamcollege/Person.java
@@ -1,3 +1,9 @@
+/**
+ * @author  Russell Waring, StudentID: 100528426
+ * @date    November 5th, 2021.
+ * @version 0.0.1
+ *          Following along to the Fall 2020 video, for in-class exercise seven.
+ */
 package ca.durhamcollege;
 
 public class Person
